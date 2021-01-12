26ad7a94f976354e07a0ab3dd1da2f26d94451c2
Owain Kenway <o.kenway@ucl.ac.uk>
2021-01-12 18:17:47 +0000

    Liston on loopback interface.
    It's my preferred configuration for things to listen on the loopback interface rather than on 0.0.0.0.

--- util.c
+++ util.c
@@ -108,7 +108,7 @@ serve(int port, void (*handlecon)(int, void*), void *arg)
 
 	memset(&server, 0, sizeof(server));
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	server.sin_port = htons(port);
 	if(bind(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0){
 		perror("error: bind");
